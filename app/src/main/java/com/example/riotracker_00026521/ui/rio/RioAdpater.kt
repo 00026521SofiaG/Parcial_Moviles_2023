9e3d0b92ed54df8e753c56c223345929e1e85f03
00026521SofiaG <00026521@uca.edu.sv>
2023-05-22 15:08:36 -0600

    Adaptador RecyclerView

---
+++ app/src/main/java/com/example/riotracker_00026521/ui/rio/RioAdpater.kt
@@ -0,0 +1,34 @@
+package com.example.riotracker_00026521.ui.rio
+
+import android.content.DialogInterface.OnClickListener
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.riotracker_00026521.R
+import com.example.riotracker_00026521.data.model.RioModel
+
+class RioAdpater(private val clickListener: (RioModel)-> Unit): RecyclerView.Adapter<RioViewHolder>() {
+    private var data : List<RioModel>? = null
+
+    fun setData(data: List<RioModel>){
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RioViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout. , parent  , false)
+        return RioViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data?.size ?:0
+    }
+
+    override fun onBindViewHolder(holder: RioViewHolder, position: Int) {
+        data?. let {
+            val river = it[position]
+            holder.bind(river, clickListener)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/riotracker_00026521/ui/rio/RioViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.riotracker_00026521.ui.rio
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RioViewHolder (itemView : View) : RecyclerView.ViewHolder(itemView) {
+}
